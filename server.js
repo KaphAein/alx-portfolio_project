85f644385b44bc551c878d5dd9886ed65cccf218
KaphAein <cherifafadaly@gmail.com>
2025-01-03 20:04:35 +0200

    amending mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 app.listen(3000, () => console.log('Server Started'))
